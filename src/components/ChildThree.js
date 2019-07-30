bef8988aad3e4685be723469bc146f0e18a29fc7
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-07-30 11:38:23 -0500

    introduce the third child component

---
+++ src/components/ChildThree.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class ChildThree extends Component {
+    render() {
+        return (
+            <div >Child three
+            </div>
+        )
+    }
+}
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ChildOne from './ChildOne';
 import ChildTwo from './ChildTwo';
+import ChildThree from './ChildThree';
 
 export default class Parent extends Component {
     state = {
@@ -15,6 +16,7 @@ export default class Parent extends Component {
             <div className="parent__container">
                 <ChildOne toggleMap={this.toggleMap} />
                 <ChildTwo isMapVisible={this.state.isMapVisible} />
+                <ChildThree />
             </div>
         )
     }
