7edd1d2f37718c26c15982833267ee4bd904da18
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-07-31 10:26:41 -0500

    add a report chart with react chart js 2 library

--- src/components/ChildThree.js
+++ src/components/ChildThree.js
@@ -1,10 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Bar } from "react-chartjs-2";
 
 export default class ChildThree extends Component {
+  constructor(props) {
+    super(props);
+    props.childThreeCallback(() => {
+      this.setState({
+        chartAsBase64: this.barChartReference.chartInstance.toBase64Image()
+      });
+    });
+  }
+    state = {};
+
+    doc = {
+        background: "#fff",
+        width: "100%",
+        minHeight: "842px",
+        padding: "25px",
+        letterSpacing: "1px",
+        fontFamily: "Roboto, sans-serif !important",
+    };
+    header = { display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0 20px" };
+    h2 = { color: "#a4a4a4", fontWeight: 700, letterSpacing: "2px" };
+    logoWrap = { width: "250px", height: "80px" };
+    logo = { width: "100%" };
+    hr = { background: "#a4a4a4" };
+    base64 = { width: "100%" };
+
     render() {
-        return (
-            <div >Child three
+        const { chartAsBase64 } = this.state
+        const { reportDataSource } = this.props
+    return (
+      <div id="report" style={this.doc}>
+        <div style={this.header}>
+          <div style={this.logogWrap}>
+            <img
+              style={this.logo}
+              src="./assets/placeholder.png"
+              alt="logo"
+            />
+          </div>
+          <h2 style={this.h2}>Yearly Report</h2>
+        </div>
+            <hr style={this.hr} />
+            <div style={this.barChartWrap} id="canvasChart">
+              <Bar
+                data={reportDataSource}
+                ref={reference => {
+                  this.barChartReference = reference;
+                }}
+                options={{ legend: { display: false } }}
+              />
             </div>
-        )
-    }
+            {chartAsBase64 && (
+              <img
+                src={chartAsBase64}
+                alt="chart"
+                className="imageForPrint"
+                style={this.base64}
+              />
+            )}
+      </div>
+    );
+  }
 }
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -6,17 +6,54 @@ import ChildThree from './ChildThree';
 export default class Parent extends Component {
     state = {
         isMapVisible: false,
+        isReportVisible: false,
     }
+
+    reportDataSource = {
+        labels: ["May", "June", "July", "August", "September", "October", "November"],
+        datasets: [
+            {
+            label: "Income Growth",
+            data: [345, 654, 739, 1034, 1281, 1575, 2027],
+            backgroundColor: ["#d1f542", "#96f542", "#42f5a4", "#42f5ec", "#42b9f5", "#426cf5", "#7842f5"],
+            },
+        ],
+    }
+
     toggleMap = () => {
         this.setState({isMapVisible: !this.state.isMapVisible})
     }
 
+    reportCallback = () => {};
+
+    handleReportView = () => {
+        this.reportCallback();
+        this.setState({ isReportVisible: true });
+    }
+
     render() {
+        const { isMapVisible, isReportVisible } = this.state;
         return (
             <div className="parent__container">
-                <ChildOne toggleMap={this.toggleMap} />
-                <ChildTwo isMapVisible={this.state.isMapVisible} />
-                <ChildThree />
+                {isReportVisible ? (
+                    <ChildThree childThreeCallback={cb => this.reportCallback = cb} reportDataSource={this.reportDataSource}/>
+                ) : (
+                    <React.Fragment>
+                        <ChildOne toggleMap={this.toggleMap} />
+                        <ChildTwo isMapVisible={isMapVisible} />
+                        <div className="report__preview-container">
+                            <button
+                                onClick={() => this.handleReportView()}
+                                className="view-button"
+                            >
+                                View the Report
+                            </button>
+                            <div className="report__thumbnail-wrap">
+                            <img className="report__thumbnail" src="./assets/report.jpg" alt="thumbnail" />
+                            </div>
+                        </div>
+                    </React.Fragment>
+                )}
             </div>
         )
     }
